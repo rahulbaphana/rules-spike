c048eeb927cb316bde1538db3e3985fed0ae1457
Rahul Baphana <rahulkb@in-rahulkb.local>
2017-12-26 16:19:39 +0530

    Reading rule from yml file

--- src/main/java/com/tw/easy/rules/TestRule.java
+++ src/main/java/com/tw/easy/rules/TestRule.java
@@ -1,32 +1,35 @@
 package com.tw.easy.rules;
 
+import com.tw.domain.Country;
+import com.tw.easy.rules.fact.Gift;
 import org.jeasy.rules.api.Facts;
 import org.jeasy.rules.api.Rule;
 import org.jeasy.rules.api.Rules;
 import org.jeasy.rules.api.RulesEngine;
 import org.jeasy.rules.core.DefaultRulesEngine;
-import org.jeasy.rules.core.RuleBuilder;
+import org.jeasy.rules.mvel.MVELRuleFactory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Map;
 
 public class TestRule {
-    public static void main(String[] args) {
-        Rule giftAmountRule = new RuleBuilder()
-                .description("Gift Amount Rule")
-                .name("Country-Amount-Rule")
-                .when(facts -> facts.<Integer>get("amountToGift") <= 100)
-                .then(facts -> System.out.println("You may gift!!"))
-                .build();
-
+    public static void main(String[] args) throws FileNotFoundException {
+        String giftRuleYmlFile = new TestRule().getRuleFromFile();
+        Rule giftMVELRule = MVELRuleFactory.createRuleFrom(new File(giftRuleYmlFile));
         Facts giftFacts = new Facts();
-        giftFacts.put("amountToGift", 90);
+        giftFacts.put("giftRule", new Gift(Country.USA, 0, 100, "Not Approved!"));
+        giftFacts.put("amountToGift", 190);
 
         RulesEngine defaultRulesEngine = new DefaultRulesEngine();
-        Map<Rule, Boolean> checkResult = defaultRulesEngine.check(new Rules(giftAmountRule), giftFacts);
-        defaultRulesEngine.fire(new Rules(giftAmountRule), giftFacts);
+        Map<Rule, Boolean> checkResult = defaultRulesEngine.check(new Rules(giftMVELRule), giftFacts);
 
         for (Rule rule : checkResult.keySet()) {
             System.out.println("Rule : "+rule.getName() + " | state : "+checkResult.get(rule));
         }
+    }
+
+    private String getRuleFromFile() {
+        return getClass().getClassLoader().getResource("gift-rule.yml").getFile();
     }
 }
--- src/test/java/TestRules.java
+++ src/test/java/TestRules.java
@@ -6,9 +6,13 @@ import org.jeasy.rules.api.Rules;
 import org.jeasy.rules.api.RulesEngine;
 import org.jeasy.rules.core.DefaultRulesEngine;
 import org.jeasy.rules.mvel.MVELRule;
+import org.jeasy.rules.mvel.MVELRuleFactory;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.Map;
 
 public class TestRules {
@@ -16,7 +20,7 @@ public class TestRules {
     @Test
     public void should_approve_for_USA_when_amount_is_less_than_100_dollars() {
         Gift giftRuleforUSA = new Gift(Country.USA, 0, 100, "Approved!");
-        MVELRule giftMVELRule = giftRuleforUSA.amountRule();
+        Rule giftMVELRule = giftRuleforUSA.amountRule();
         Facts giftFacts = new Facts();
         giftFacts.put("giftRule", giftRuleforUSA);
         giftFacts.put("amountToGift", 90);
@@ -30,9 +34,23 @@ public class TestRules {
     @Test
     public void should_unapprove_for_USA_when_amount_is_greater_than_100_dollars() {
         Gift giftRuleforUSA = new Gift(Country.USA, 0, 100, "Not Approved!");
-        MVELRule giftMVELRule = giftRuleforUSA.amountRule();
+        Rule giftMVELRule = giftRuleforUSA.amountRule();
         Facts giftFacts = new Facts();
         giftFacts.put("giftRule", giftRuleforUSA);
+        giftFacts.put("amountToGift", 190);
+
+        RulesEngine defaultRulesEngine = new DefaultRulesEngine();
+        Map<Rule, Boolean> checkResult = defaultRulesEngine.check(new Rules(giftMVELRule), giftFacts);
+
+        Assert.assertFalse(checkResult.get(giftMVELRule));
+    }
+
+    @Test
+    public void should_load_rule_from_file() throws FileNotFoundException {
+        String giftRuleYmlFile = getClass().getClassLoader().getResource("gift-rule.yml").getFile();
+        Rule giftMVELRule = MVELRuleFactory.createRuleFrom(new File(giftRuleYmlFile));
+        Facts giftFacts = new Facts();
+        giftFacts.put("giftRule", new Gift(Country.USA, 0, 100, "Not Approved!"));
         giftFacts.put("amountToGift", 190);
 
         RulesEngine defaultRulesEngine = new DefaultRulesEngine();
